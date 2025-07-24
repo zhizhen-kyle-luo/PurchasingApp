21483f971d5afb432c9df034fc5be55eb4345829
zhizhen-kyle-luo <zhizhen.luo07@gmail.com>
2025-07-24 12:02:13 +0545

    feat(change-name-modal): add working modal with update/cancel/close actions and event output for name change

--- frontend/src/app/components/change-name-modal/change-name-modal.component.ts
+++ frontend/src/app/components/change-name-modal/change-name-modal.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-change-name-modal',
+  selector: "app-change-name-modal",
   standalone: true,
-  imports: [],
-  templateUrl: './change-name-modal.component.html',
-  styleUrl: './change-name-modal.component.scss'
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./change-name-modal.component.html",
+  styleUrls: ["./change-name-modal.component.scss"],
 })
 export class ChangeNameModalComponent {
+  @Input() showModal = false;
+  @Output() closeModalEvent = new EventEmitter<void>();
+  @Output() nameUpdated = new EventEmitter<string>();
 
+  newName = "";
+
+  closeModal() {
+    this.closeModalEvent.emit();
+    this.newName = "";
+  }
+
+  updateName() {
+    if (this.newName.trim()) {
+      this.nameUpdated.emit(this.newName.trim());
+    }
+    this.closeModal();
+  }
 }
